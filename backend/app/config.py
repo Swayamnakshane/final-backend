3313f7b81526b2bfdd88c99de6149d7c7445ee06
Ubuntu <ubuntu@ip-172-31-2-115.us-east-2.compute.internal>
2025-06-30 13:01:54 +0000

    k8s added

--- backend/app/config.py
+++ backend/app/config.py
@@ -9,7 +9,7 @@ class Config:
     MYSQL_USER = os.getenv('MYSQL_USER')
     MYSQL_PASSWORD = urllib.parse.quote_plus(os.getenv('MYSQL_PASSWORD'))
     MYSQL_HOST = os.getenv('MYSQL_HOST')
-    MYSQL_DB = os.getenv('MYSQL_DB')
+    MYSQL_DB = os.getenv('MYSQL_DATABASE')
 
     SQLALCHEMY_DATABASE_URI = (
         f"mysql+pymysql://{MYSQL_USER}:{MYSQL_PASSWORD}@{MYSQL_HOST}/{MYSQL_DB}"
