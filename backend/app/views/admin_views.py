3233bcbc43b7f35087f9fae67250ba3a454f7ac7
Swayam Pradeep Nakshane <nakshaneswayamatha24@gmail.com>
2025-07-17 14:42:15 +0530

    Update admin_views.py

--- backend/app/views/admin_views.py
+++ backend/app/views/admin_views.py
@@ -537,7 +537,7 @@ SMTP_SERVER = 'arcap.info'
 SMTP_PORT = 465  # SSL
 SMTP_EMAIL = 'hra@arcap.info'
 SMTP_PASSWORD = 'Ganesh@arcap2025'
-EXAM_LINK = 'http://34.219.21.193:3000/'
+EXAM_LINK = 'https://34.219.21.193.nip.io:8080/'
 IST = pytz.timezone('Asia/Kolkata')
 
 
